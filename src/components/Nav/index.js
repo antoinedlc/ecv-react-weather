ca7bf0fcdc07c0aa66d624a56dd3d96343354766
Antoine Delcourte <adelcourte@siliconsalad.com>
2022-02-25 16:20:53 +0100

    Improving nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,10 +4,14 @@ import './index.scss'
 export default function Nav() {
     return(
         <nav className="nav glass">
-            <Link to="/" className="nav__link">Home</Link>
-            <Link to="/search" className="nav__link">Search</Link>
-            <Link to="/saved" className="nav__link">Saved</Link>
-            <Link to="/map" className="nav__link">Map</Link>
+            <div className="nav__logo"></div>
+            <div className="nav__links">
+                <Link to="/" className="nav__link">Home</Link>
+                <Link to="/search" className="nav__link">Search</Link>
+                <Link to="/saved" className="nav__link">Saved</Link>
+                <Link to="/map" className="nav__link">Map</Link>
+            </div>
+            <div className="nav__copyright">© adelcourte - 2022</div>
         </nav>
     )
 }
